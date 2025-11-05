2e0faf27e6a0d199899ffc3ed16f401d7472ff7f
Felix Vincent <vincentvirtuoso66@gmail.com>
2025-11-05 06:37:24 -0800

    fix ui errors

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ const PORT = process.env.PORT || 4000;
 const allowedOrigins = [
   "http://localhost:5173",
   "https://propertiesinnigeriatvicl.onrender.com",
-  "https://www.tvicloffice.com",
+  "https://www.tvicl.com.ng",
 ];
 
 app.use((req, res, next) => {
